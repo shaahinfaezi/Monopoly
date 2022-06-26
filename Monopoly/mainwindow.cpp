455690a8b0503b42e94775b662b57ca0e312181e
shaahinfaezi <shaahinfaezi@gmail.com>
2022-06-26 18:58:12 +0430

    testing

--- Monopoly/mainwindow.cpp
+++ Monopoly/mainwindow.cpp
@@ -1,12 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "gameboard.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QPixmap logo("");
+
+
+
 }
 
 MainWindow::~MainWindow()
